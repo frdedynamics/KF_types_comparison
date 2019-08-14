1aba7e7b3fa45505642c500d9fb369a73c4a583d
Gizem Ates <gizemates@iyte.edu.tr>
2019-08-14 18:02:30 +0200

    Report for KalmanManual is ready

--- read_log_script.m
+++ read_log_script.m
@@ -1,21 +1,15 @@
-% This script reads in the specified text file logged by "sensorlog.txt"
+%% Read Log Script
+% Read sensor data from a text file and saves them into a .mat file in
 
+%% Fresh workspace at first
 clear all
 close all
 
-% Specify name of input file
-
-
-input_filename='sensorLog_2pitch.txt';
-output_filename='data.mat';
-
-
-
-
-
+%% Import log file
+% Specify name of input file that you saved using your smart phone.
+input_filename = 'sensorLog_2pitch.txt';
+output_filename = 'data.mat';
 fid=fopen(input_filename);
-
-
 
 % The next lines are in the format
 % data_type time data_x data_y data_z
@@ -25,53 +19,50 @@ fid=fopen(input_filename);
 % data_x, data_y, data_z = floating-point values of vector of above
 % Note units depend on type
 
-data=textscan(fid,'%f %s %f %f %f');  % Resulting format: cells
-
+data = textscan(fid,'%f %s %f %f %f');  % Resulting format: cells
 fclose(fid);
 
-% Extract out numerical entries of data into an array (used below)
-data_matrix=cell2mat( data(:,[1,3,4,5]) );
+%% Extract out numerical entries of data into an array (used below)
+data_matrix = cell2mat( data(:,[1,3,4,5]) );
 
-% Now scan through A{1} (a cell) looking for each of the five types
-
-
-
-test=strcmp(data{2},'ACC');  
-test1=find(test==true);  
-accel_data=data_matrix(test1,:); 
-
-test=strcmp(data{2},'GYR');  
-test1=find(test==true); 
-gyro_data=data_matrix(test1,:); 
-
-test=strcmp(data{2},'MAG'); 
-test1=find(test==true);  
-mag_data=data_matrix(test1,:); 
+%% Now scan through A{1} (a cell) looking for each of type. 
+% We will implement Kalman filter only using accelerometer and gyroscope
+% data. Therefore we check only two types.
+test = strcmp(data{2},'ACC');  
+test1 = find(test==true);  
+accel_data = data_matrix(test1,:); 
+accel_data_length = length(accel_data);
 
 
+test = strcmp(data{2},'GYR');  
+test1 = find(test==true); 
+gyro_data = data_matrix(test1,:); 
+gyro_data_length = length(gyro_data);
 
+% accelerometer starts logging earlier than gyroscope. With the following
+% line we crop the first elements of the accelerometer where the gyroscope
+% has not started yet. By doing that we make sure the length of accel_data
+% and gyro_data are equal.
+accel_data = accel_data(length(accel_data)+1-min(length(accel_data),length(gyro_data)):end,:);
 
+% Our mobile app logs the data in each 10ms. We set this value here and
+% create our accelerometer and gyroscope timeseries
+sample_time = 0.01; 
+sim_time = (sample_time*(length(accel_data)-1)); 
+t_data = 0:sample_time:sim_time; 
+acc_ts = timeseries(accel_data(:,2:4), t_data, 'name', 'Accelerometer'); 
+gyro_ts = timeseries(gyro_data(:,2:4), t_data, 'name', 'Gyroscope'); 
 
+%% Save all to disk as .mat file to be able to use in simulink
+save('Accelerometer','acc_ts','-v7.3')
+save('Gyroscope','gyro_ts','-v7.3')
 
-% Save all to disk (to avoid rerunning the script - is this useful?)
-
-save(output_filename,'accel_data','gyro_data','mag_data');
-
-%adaptation to other code
-accel=accel_data;
-clear accel_data;
-gyro=gyro_data;
-clear gyro_data;
-magneto=mag_data;
-clear mag_data;
-clear attitude_data;
+%% flush the memory after we are done.
 clear data;
 clear fid
 clear output_filename
 clear test
 clear test1
-
-
 disp([' Data from ' input_filename  'saved']);
 
 
